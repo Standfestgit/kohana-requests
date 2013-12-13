97293a48aa32e565c1a060f4dfd3c8e01e8ea9f4
Alexander Standfest <standfest@teleglobal.lv>
2013-12-13 22:50:16 +0200

    added including of Requests library

--- init.php
+++ init.php
@@ -1,4 +1,3 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-
-
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'requests'.DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'Requests'.EXT);
